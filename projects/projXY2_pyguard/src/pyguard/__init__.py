1d4f95a6f4d54a56fafa21625729f8b0d545e5da
fixcfhu <julius.pravtchev@cariad.technology>
2026-08-04 11:04:19 +0200

    Bobs Server

--- projects/projXY2_pyguard/src/pyguard/__init__.py
+++ projects/projXY2_pyguard/src/pyguard/__init__.py
@@ -1,3 +1,9 @@
 from .middleware import PyGuardMiddleware
+from .models     import Request, ScanResult
+from .exceptions import RequestBlocked
 
-__all__ = ["PyGuardMiddleware"]
+__all__ = ["PyGuardMiddleware",
+           "Request",
+           "ScanResult",
+           "RequestBlocked",
+           ]
